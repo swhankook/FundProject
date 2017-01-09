bd4555260018326fc8dc4f30267711524ede0c1f
jun <jun@192.168.0.105>
2017-01-09 19:22:34 +0900

    q

---
+++ Fund/src/main/java/www/sample/dao/SampleDAO.java
@@ -0,0 +1,18 @@
+package www.sample.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import www.common.dao.AbstractDAO;
+
+@Repository("sampleDAO")
+public class SampleDAO extends AbstractDAO{
+	
+	@SuppressWarnings("unchecked")
+    public List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception{
+        return (List<Map<String, Object>>)selectList("sample.selectBoardList", map);
+    }
+
+}
