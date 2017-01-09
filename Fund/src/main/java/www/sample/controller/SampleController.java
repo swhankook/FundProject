8a4b9914d889151b5c66f3475ba9420fca527558
swhankook <newkorea88@naver.com>
2017-01-09 21:43:27 +0900

    sample page test

--- Fund/src/main/java/www/sample/controller/SampleController.java
+++ Fund/src/main/java/www/sample/controller/SampleController.java
@@ -8,19 +8,21 @@ import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import www.sample.service.SampleService;
 
 @Controller
+@RequestMapping("/sample")
 public class SampleController {
 	private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
 
     @Resource(name="sampleService")
     private SampleService sampleService;
 
-    @RequestMapping(value="/sample/openSampleBoardList")
+    @RequestMapping(value="/openSampleBoardList.do")
     public ModelAndView openSampleList(Map<String,Object> commandMap) throws Exception{
         ModelAndView mv = new ModelAndView("/sample/boardList");
 
@@ -28,5 +30,10 @@ public class SampleController {
         mv.addObject("list", list);
 
         return mv;
+    }
+
+    @RequestMapping(value="/sample.do")
+    public String sample(Model model) throws Exception{
+    	return "sample";
     }
 }
