f43a04eb21142561ea9e4485338463bc41f5bf1a
swhankook <newkorea88@naver.com>
2017-02-16 23:57:02 +0900

    loan

---
+++ Fund/src/main/java/www/common/common/Loan.java
@@ -0,0 +1,103 @@
+package www.common.common;
+
+public class Loan {
+	private int idx;
+	private int parent_idx;
+	private String purpose;
+	private int money;
+	private int period;
+	private int income;
+	private String name;
+	private int birthday;
+	private String email;
+	private int phone;
+	private String sex;
+
+	public int getIdx() {
+		return idx;
+	}
+
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+
+	public int getParent_idx() {
+		return parent_idx;
+	}
+
+	public void setParent_idx(int parent_idx) {
+		this.parent_idx = parent_idx;
+	}
+
+	public String getPurpose() {
+		return purpose;
+	}
+
+	public void setPurpose(String purpose) {
+		this.purpose = purpose;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+
+	public int getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(int period) {
+		this.period = period;
+	}
+
+	public int getIncome() {
+		return income;
+	}
+
+	public void setIncome(int income) {
+		this.income = income;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(int birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+}
